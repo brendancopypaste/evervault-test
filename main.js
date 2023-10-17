84011d87c8cd49da4e97b20fbf6522dd923f658f
brendancopypaste <33678071+brendancopypaste@users.noreply.github.com>
2023-10-17 10:06:07 -0500

    add logging

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const playwright = require("playwright");
 
 module.exports.handler = async (event, context) => {
+  console.log("Launching browser...");
   const browser = await playwright.chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  console.log("Creating new context...");
+  const browserContext = await browser.newContext();
+  console.log("Opening new page...");
+  const page = await browserContext.newPage();
+  console.log("Navigating to https://www.google.com...");
   await page.goto("https://www.google.com");
-  const cookies = await context.cookies();
+  console.log("Getting cookies...");
+  const cookies = await browserContext.cookies();
+  console.log("Closing browser...");
   await browser.close();
+  console.log("Returning response...");
   return {
     statusCode: 200,
     body: JSON.stringify(cookies),
